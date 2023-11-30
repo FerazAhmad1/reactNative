550dacbac405479439f81568e94747845c90c62f
Feraz Khan <ferazahmadgit62@gmail.com>
2023-12-01 00:28:09 +0530

    Text component

--- coreComponents/App.js
+++ coreComponents/App.js
@@ -1,5 +1,11 @@
 import { View, Text } from "react-native";
 
 export default function app() {
-  return <View style={{ flex: 1, backgroundColor: "plum" }}>Hello</View>;
+  return (
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <Text style={{ color: "red", margin: "50px" }}>
+        Hello from mobile app
+      </Text>
+    </View>
+  );
 }

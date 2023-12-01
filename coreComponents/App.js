10ab4620c378a0f8bb9e8067a9219efa45f33132
Feraz Khan <ferazahmadgit62@gmail.com>
2023-12-02 01:09:11 +0530

    button component

--- coreComponents/App.js
+++ coreComponents/App.js
@@ -1,12 +1,18 @@
-import { View, Text, ImageBackground, ScrollView } from "react-native";
+import { View, Text, ImageBackground, ScrollView, Button } from "react-native";
 import Hello from "./TextComponent";
 import ImageComponent from "./ImageComponent";
 import logoImage from "./assets/adaptive-icon.png";
 export default function app() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Hello />
-      <ImageComponent />
+      {/* <Hello />
+      <ImageComponent /> */}
+      <Button
+        title="press"
+        onPress={() => console.log("i got press")}
+        color="midnightblue"
+        disabled
+      />
     </View>
   );
 }
